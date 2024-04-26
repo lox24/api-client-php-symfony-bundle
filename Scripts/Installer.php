63fe50b6cc104509f710da1477f79145e7251b9a
Andrey Lebedev <andrew.lebedev@gmail.com>
2024-04-26 14:47:10 +0400

    initial code version

--- Scripts/Installer.php
+++ Scripts/Installer.php
@@ -10,13 +10,15 @@ use lox24\bundle\api_client\LOX24ApiClientBundle;
 readonly final class Installer {
     public static function postInstall(): void
     {
-        $bundlesPath = __DIR__ . '/../../../config/bundles.php';
-        self::appendToBundlesPhp($bundlesPath, LOX24ApiClientBundle::class);
+        $configDirPath = __DIR__ . '/../../../config/';
+        self::appendToBundlesPhp($configDirPath, LOX24ApiClientBundle::class);
+        self::appendToBundlesPhp($configDirPath, LOX24ApiClientBundle::class);
     }
 
-    private static function appendToBundlesPhp(string $bundlesPath, string $bundleClass, array $envs = ['all' => true]): void
+    private static function appendToBundlesPhp(string $path, string $bundleClass, array $envs = ['all' => true]): void
     {
-        $contents = file_get_contents($bundlesPath);
+        $path .= '/bundles.php';
+        $contents = file_get_contents($path);
         if($contents === false) {
             throw new \RuntimeException('Could not read bundles.php');
         }
@@ -24,16 +26,16 @@ readonly final class Installer {
 
         if (!str_contains($contents, $newLine)) {
             $contents = str_replace("];", "$newLine];", $contents);
-            file_put_contents($bundlesPath, $contents);
+            file_put_contents($path, $contents);
         }
     }
 
-    private static function createYamlConfig(string $bundlesPath): void
+    private static function createYamlConfig(string $path): void
     {
-        $configPath = 'path/to/config/packages/mybundle.yaml';
-        if (!file_exists($configPath)) {
-            $yamlContent = "services:\n  my_service:\n    class: MyClass\n";
-            file_put_contents($configPath, $yamlContent);
+        $path .= 'packages/lox24.yaml';
+        if (!file_exists($path)) {
+            $contents = "parameters:\n  lox24:\n    class: MyClass\n";
+            file_put_contents($path, $contents);
         }
     }
 
